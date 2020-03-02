3b02cc0bc7bae9bf387694d99a3637fb912811fc
Merle <mikezh2011@163.com>
2020-03-02 04:03:59 +0000

    v0.5

--- BookScan/BookScanning.java
+++ BookScan/BookScanning.java
@@ -54,6 +54,9 @@ public class BookScanning {
         HashSet<Book> scannedBook = new HashSet<Book>();
         for (Library library : libraries) {
             library.removedup(scannedBook);
+            if (library.books.size() == 0) {
+                continue;
+            }
             System.out.println(library.id + " " + library.books.size());
             Collections.sort(library.books, Collections.reverseOrder());
             for (Book book : library.books) {
